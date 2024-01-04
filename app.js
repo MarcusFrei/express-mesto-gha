7540af1b676e971f96f9d7a275249f4a90d0a5cf
MarcusFrei <133006996+MarcusFrei@users.noreply.github.com>
2024-01-04 12:57:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,27 +4,24 @@ const app = express();
 const PORT = 3000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const httpStatusCodes = require('./errors/errors');
+const auth = require('./middlewares/auth');
+const { login, createUser } = require('./controllers/users');
 
 const DB = 'mongodb://127.0.0.1:27017/mestodb';
 mongoose.connect(DB);
 const router = require('./routes');
 const { NOT_FOUND_ROUTE } = require('./const');
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '65703da48dbd9adf8885f9e6',
-  };
-
-  next();
-});
-
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.listen(PORT);
-
-app.use('/users', router.users);
+app.post('/signin', login);
+app.post('/signup', createUser);
+app.use('/users', auth, router.users);
 app.use('/cards', router.cards);
 app.use('*', (req, res) => {
   res.status(httpStatusCodes.NOT_FOUND).send({ message: NOT_FOUND_ROUTE });
